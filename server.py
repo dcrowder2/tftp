71be57db72ae70870c254dc0bde9887d6e96b5c8
dcrowder2 <dcrowder2@alaska.edu>
2018-09-27 10:58:27 -0800

    Server program for tftp

---
+++ server.py
@@ -0,0 +1,37 @@
+from net import Net
+
+
+class Server(Net):
+
+	def __init__(self):
+		# Run super initializer
+		Net.__init__(self)
+
+		# add flags for server into argparse
+		self.parser.add_argument('-v', action='store_true', help='Flag for verbose mode')
+
+		# parse the arguments
+		args = self.parser.parse_args()
+
+		# bind the socket to the port
+		self.sock.bind(('', args.p))
+		self.sock.listen(1)
+		if args.v:
+			print("Server IP: " + str(self.sock.getsockname()) + "\nPort number: " + str(args.p) + "\nWaiting for connection")
+		else:
+			print("Ready to recieve")
+		while True:
+			connection_socket, address = self.sock.accept()
+
+			if args.v:
+				print("Connection made with " + str(address))
+
+			data = connection_socket.recv(516)
+
+			connection_socket.close()
+			break
+
+
+if __name__ == '__main__':
+	server = Server()
+
