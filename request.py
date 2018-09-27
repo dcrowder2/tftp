007fa41017f821c61d8b7403d645727b4e58b8b1
dcrowder2 <dcrowder2@alaska.edu>
2018-09-27 11:32:11 -0800

    Added formatting the packet for a read/write request

--- request.py
+++ request.py
@@ -3,6 +3,18 @@ from header import Header
 
 class Request(Header):
 
-	def __init__(self, code):
+	def __init__(self, code, filename, mode):
 
 		Header.__init__(self, code)
+
+		self.file = bytearray(filename.encode('utf-8'))
+
+		self.head += self.file
+
+		self.head.append(0)
+
+		self.mode = bytearray(mode.encode('utf-8'))
+
+		self.head += self.mode
+
+		self.head.append(0)
