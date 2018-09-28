ba404d7fd046d980bac1a4b281f0a3578f9f0e0c
dcrowder2 <dcrowder2@alaska.edu>
2018-09-28 09:47:32 -0800

    Added ability to create a data packet, a request packet and read from request and data packet

--- packet.py
+++ packet.py
@@ -5,8 +5,34 @@ from request_packet import Request
 
 class Packet:
 
-	def __init__(self):
-		self.block_number = 0
+	@staticmethod
+	def request(filename, mode, write_flag):
+		if write_flag:
+			new_packet = Request(2, filename, mode).packet
+		else:
+			new_packet = Request(1, filename, mode).packet
+		return new_packet
 
-	def request(self, filename, mode):
-		
+	@staticmethod
+	def data(block_number, data_line):
+		new_packet = Datack(3, block_number).packet
+		new_packet += bytearray(data_line)
+		return new_packet
+
+	@staticmethod
+	def read_packet(packet):
+		op_code = packet[1]
+		return_info = []
+		if op_code == 1 or op_code == 2:
+			return_info.append(packet[1])
+			return_info.append(packet[3])
+		elif op_code == 3:
+			# The block number is byte 3 and 4 of the packet, and should be added together to get the correct number
+			return_info.append(packet[2] + packet[3])
+			# Then the next part is the data chunk from the file
+			return_info.append(packet[4:])
+		elif op_code == 4:
+			return_info.append(packet[1])
+		else:
+			return_info.append(packet[1])
+		return return_info
