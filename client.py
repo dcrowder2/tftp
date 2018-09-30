57f0fb4e931f409c7262384a3d78ec6061945722
dcrowder2 <dcrowder2@alaska.edu>
2018-09-30 10:15:54 -0800

    Added client side checking for the correct filename

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from net import Net
 from packet import Packet
+from os import path
 
 
 class Client(Net):
@@ -16,6 +17,10 @@ class Client(Net):
 
 		# parse the arguments
 		args = self.parser.parse_args()
+
+		if not path.exists(args.f):
+			print("File not found, please enter a valid filename")
+			exit(0)
 
 		# Connect to the server
 		self.sock.connect((args.a, args.p))
