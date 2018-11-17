d09d69aec0e364239454cbed2e5fd190aeb17bc3
dcrowder2 <dcrowder2@alaska.edu>
2018-11-17 13:18:18 -0900

    Added passing through of Final Packet

--- datack_packet.py
+++ datack_packet.py
@@ -3,20 +3,19 @@
 # University of Alaska Anchorage
 # Trivial File Transport Protocol
 from header import Header
-from error_packet import Error
 import bitstring
 
 
 # This class encompasses the duties for a data packet and a ack packet
 class Datack(Header):
 
-	def __init__(self, in_code, sequence_number, ack_number, d_port, s_port, data=b''):
+	def __init__(self, in_code, sequence_number, ack_number, d_port, s_port, data=b'', fin=False):
 
 		if in_code == 4:  # ack packet
 			Header.__init__(self, sequence_number, s_port, d_port, ack_number, ack=True)
 
 		if in_code == 3:  # data packet
-			Header.__init__(self, sequence_number, s_port, d_port, ack_number)
+			Header.__init__(self, sequence_number, s_port, d_port, ack_number, fin=fin)
 			self.data = bitstring.BitArray(data)
 
 	def combine(self):
