0de311038890e00f615c19c98c2213566bac55a0
dcrowder2 <dcrowder2@alaska.edu>
2018-11-18 09:59:51 -0900

    Changed over request, data, and error packet creation to the new header, as well as added the 5% corruption for data packets

--- packet.py
+++ packet.py
@@ -5,36 +5,44 @@
 from datack_packet import Datack
 from error_packet import Error
 from request_packet import Request
+import random
+from bitstring import Bits
 
 
 class Packet:
 
 	@staticmethod
-	def find_zero(bytearr):
-		index = 0
-		for byte in bytearr:
-			if byte == 0:
-				break
-			else:
-				index += 1
-		return index + 2
+	def corrupt(packet):
+		d100_roll = random.randint(0, 100)
+		if d100_roll <= 5:
+			# To corrupt the checksum I set it to the binary representation for the word no, which is 16 bits,
+			# and while it is possible for the checksum to be this, it is very slim chances so it should be fine
+			packet.checksum = Bits(b'No')
 
 	@staticmethod
-	def request(filename, mode, write_flag):
+	def request(filename, write_flag, d_port, s_port, seq_num):
 		if write_flag:
-			new_packet = Request(2, filename, mode).packet
+			new_packet = Request(2, filename, seq_num, s_port, d_port)
 		else:
-			new_packet = Request(1, filename, mode).packet
-		return new_packet
+			new_packet = Request(1, filename, seq_num, s_port, d_port)
+		# after initialization checksum needs to be calculated
+		new_packet.calc_checksum()
+		# and we return the bitarray for the packet
+		return new_packet.combine()
 
 	@staticmethod
-	def data(sequence_number, data_line):
-		new_packet = Datack(3, sequence_number, data_line)
-		return new_packet
+	def data(sequence_number, data_line, s_port, d_port, ack_num, fin=False):
+		new_packet = Datack(3, sequence_number, ack_num, d_port, s_port, data_line, fin=fin)
+		new_packet.calc_checksum()
+		# Data packets are to be corrupted with a 5% probability, so we need to roll the die
+		Packet.corrupt(new_packet)
+		return new_packet.combine()
 
 	@staticmethod
-	def error(error_code, in_message=''):
-		return Error(error_code, in_message).packet
+	def error(error_code, d_port, s_port, seq_num, in_message=''):
+		new_packet = Error(error_code, d_port, s_port, seq_num, in_message)
+		new_packet.calc_checksum()
+		return new_packet.combine()
 
 	@staticmethod
 	def ack(sequence_number):
@@ -42,7 +50,7 @@ class Packet:
 
 	@staticmethod
 	def kill():
-		return bytearray([0,9])
+		return bytearray([0, 9])
 
 	@staticmethod
 	def read_packet(packet):
